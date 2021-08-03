bb308e3770377be40952880caf5048b9e6677714
bgi08 <77622579+bgi08@users.noreply.github.com>
2021-08-03 20:39:12 +0530

    Changes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,18 +2,55 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-MyCanvas myCanvas;
+    MyCanvas myCanvas;
+Button easygame,mediumgame,hardgame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myCanvas=new MyCanvas(this);
-        myCanvas.setBackgroundColor(Color.WHITE);
-        setContentView(myCanvas);
+        easygame=findViewById(R.id.button);
+        mediumgame=findViewById(R.id.button2);
+        hardgame=findViewById(R.id.button3);
 
+
+
+        easygame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas=new MyCanvas(MainActivity.this);
+                setContentView(myCanvas);
+
+            }
+        });
+        mediumgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas=new MyCanvas(MainActivity.this);
+                setContentView(myCanvas);
+
+
+            }
+        });
+        hardgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myCanvas=new MyCanvas(MainActivity.this);
+                setContentView(myCanvas);
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
--- app/src/main/java/com/example/myapplication/MyCanvas.java
+++ app/src/main/java/com/example/myapplication/MyCanvas.java
@@ -1,12 +1,18 @@
 package com.example.myapplication;
 
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.os.Vibrator;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,29 +23,29 @@ import androidx.annotation.Nullable;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MyCanvas extends View {
+
+
     Paint borderpaint = new Paint();
     Paint textpaint = new Paint();
     Paint cellpaint = new Paint();
     Paint backgroundPaint = new Paint();
-    int score=0;
-    int highScore=0;
-    int level=0;
+    int score = 0;
+    int highScore = 0;
+    int level = 0;
     boolean[][] isOpened = new boolean[8][8];
     boolean[][] isMine = new boolean[8][8];
-    Rect[][] cells = new Rect[8][8];
-    Paint mine,numbers= new Paint();
+    RectF[][] cells = new RectF[8][8];
+    Paint mine, numbers = new Paint();
 
     Typeface typeface = Typeface.create("Arial", Typeface.BOLD);
-    Canvas canvas1=new Canvas();
+    Canvas canvas1 = new Canvas();
+
     public MyCanvas(Context context) {
         super(context);
         init(null);
-
-
     }
 
     public MyCanvas(Context context, AttributeSet attrs) {
-
         super(context, attrs);
     }
 
@@ -60,9 +66,6 @@ public class MyCanvas extends View {
         int width = 50;
         int height = 500;
 
-
-/*
-        */
         borderpaint.setStyle(Paint.Style.STROKE);
         borderpaint.setColor(Color.BLACK);
         borderpaint.setStrokeWidth(5);
@@ -73,32 +76,18 @@ public class MyCanvas extends View {
         canvas.drawText("MINESWEEPER", 250, 100, textpaint);
         textpaint.setTextSize(50);
 
-        canvas.drawText("Score : "+score, 40, 200, textpaint);
-        canvas.drawText("Mines : "+level*8, 40,260, textpaint);
-        canvas.drawText("High Score : "+highScore, 650, 200, textpaint);
+        canvas.drawText("Score : " + score, 40, 200, textpaint);
+        canvas.drawText("Mines : " + level * 8, 40, 260, textpaint);
+        canvas.drawText("High Score : " + highScore, 650, 200, textpaint);
 
         canvas.drawRect(50, 500, 1050, 1500, borderpaint);
-/*        canvas.drawLine(175, 500, 175, 1500, borderpaint);//VERTICAL LINES
-        canvas.drawLine(300, 500, 300, 1500, borderpaint);
-        canvas.drawLine(425, 500, 425, 1500, borderpaint);
-        canvas.drawLine(550, 500, 550, 1500, borderpaint);
-        canvas.drawLine(675, 500, 675, 1500, borderpaint);
-        canvas.drawLine(800, 500, 800, 1500, borderpaint);
-        canvas.drawLine(925, 500, 925, 1500, borderpaint);
-        canvas.drawLine(50, 625, 1050, 625, borderpaint);//HORIZONTAL LINES
-        canvas.drawLine(50, 750, 1050, 750, borderpaint);
-        canvas.drawLine(50, 875, 1050, 875, borderpaint);
-        canvas.drawLine(50, 1000, 1050, 1000, borderpaint);
-        canvas.drawLine(50, 1125, 1050, 1125, borderpaint);
-        canvas.drawLine(50, 1250, 1050, 1250, borderpaint);
-        canvas.drawLine(50, 1375, 1050, 1375, borderpaint);*/
         cellpaint.setColor(Color.GRAY);
 
         for (int i = 0; i <= 7; i++) {
             for (int j = 0; j < cells[i].length; j++) {
 
-                cells[i][j] = new Rect(width, height, width + 120, height + 120);
-                 canvas.drawRect(cells[i][j], cellpaint);
+                cells[i][j] = new RectF(width, height, width + 120, height + 120);
+                canvas.drawRect(cells[i][j], cellpaint);
                 width += 125;
             }
             height += 125;
@@ -106,8 +95,8 @@ public class MyCanvas extends View {
 
         }
 
-        canvas1=canvas;
-        placemines();
+
+
 
     }
 
@@ -120,44 +109,32 @@ public class MyCanvas extends View {
     public boolean onTouchEvent(MotionEvent event) {
         int touchX = (int) event.getX();
         int touchY = (int) event.getY();
-        mine.setStrokeWidth(10);
-        mine.setColor(Color.RED);
-        mine.setStyle(Paint.Style.FILL);
-        numbers.setStrokeWidth(10);
-        numbers.setColor(Color.GREEN);
-        numbers.setStyle(Paint.Style.FILL);
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-
                 if (cells[i][j].contains(touchX, touchY)) {
+                    if (isMine[i][j]) {
+                        Toast.makeText(getContext(), "Game Over", Toast.LENGTH_SHORT).show();
+                        isOpened[i][j] = true;
 
-                    int pos = i * 8 + (j + 1);
-                    Toast.makeText(getContext(), "Clicked Rectangle "+pos, Toast.LENGTH_SHORT).show();
+                        // Vibration
+                        Vibrator v = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
+                        v.vibrate(300);
+
+                        // Setting score and high score
+
+
+                    } else {
+                        // Increasing Score and marked block as open
+
+                        isOpened[i - 1][j] = true;
+                    }
+                    invalidate();
+
                 }
-
             }
         }
         return true;
     }
-public void placemines()
-{int temp=0;
-    for(int i=1;i<=8;i++) {
-        for (int j = 0; j < 8; j++) {
-            isOpened[i-1][j]=false;
-            isMine[i-1][j]=false;
-        }
-    }
-    level=1;
-    while(temp<level*8){
-        int x = ThreadLocalRandom.current().nextInt(0, 7 + 1);
-        int y = ThreadLocalRandom.current().nextInt(0, 7 + 1);
-        if(!isMine[x][y]){
-            temp++;
-            isMine[x][y]=true;
-        }
-    }
+
+
 }
-}
-
-
-
